47cf9e53611932a0a2392af0af92de054a42aaa0
Kishan Raj <52540375+kishanRaj10@users.noreply.github.com>
2021-04-29 19:14:07 +0530

    Add files via upload

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '6ec9a440441871394965';
+const githubClientSecret = 'd108ac2a659587a28902b5955f77c69946e54c7b';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
